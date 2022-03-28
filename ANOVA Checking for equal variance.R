da92cf0d14069177c1764c364177b7f05b64db13
akthiersch-01 <97471521+akthiersch-01@users.noreply.github.com>
2022-03-28 11:35:46 -0400

    Create ANOVA Checking for equal variance.R

---
+++ ANOVA Checking for equal variance.R
@@ -0,0 +1,16 @@
+#Checking for equal Variance
+var(ranked_MW$mental_health_days, na.rm=TRUE)
+var(ranked_MW$mental_distress_rate, na.rm=TRUE)
+var(ranked_MW$inequality, na.rm =TRUE)
+
+var(ranked_WE$mental_health_days, na.rm=TRUE)
+var(ranked_WE$mental_distress_rate, na.rm=TRUE)
+var(ranked_WE$inequality, na.rm=TRUE)
+
+var(ranked_NE$mental_health_days, na.rm=TRUE)
+var(ranked_NE$mental_distress_rate, na.rm=TRUE)
+var(ranked_NE$inequality, na.rm=TRUE)
+
+var(ranked_SO$mental_health_days, na.rm=TRUE)
+var(ranked_SO$mental_distress_rate, na.rm=TRUE)
+var(ranked_SO$inequality, na.rm=TRUE)
